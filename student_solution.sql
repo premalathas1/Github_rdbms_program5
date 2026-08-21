8ac11bc245d7e29123b93c54398054e1f2e650fb
premalathas1 <premalathas@rvsgroup.com>
2026-08-21 15:25:00 +0530

    Create student_solution.sql

---
+++ student_solution.sql
@@ -0,0 +1,10 @@
+INSERT INTO Student (StudentID, StudentName, Gender, DepartmentID)
+VALUES (1001, 'Arun', 'Male', 101);
+
+INSERT INTO Student (StudentID, StudentName, Gender, DepartmentID)
+VALUES (1002, 'Divya', 'Female', 102);
+
+INSERT INTO Student (StudentID, StudentName, Gender, DepartmentID)
+VALUES (1003, 'Karthik', 'Male', 101);
+
+SELECT * FROM Student;
