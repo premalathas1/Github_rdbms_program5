020fe00fa219ec38d4271702bdae5e1908ceee1c
premalathas1 <premalathas@rvsgroup.com>
2026-08-21 15:29:42 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,10 +1,1 @@
-INSERT INTO Student (StudentID, StudentName, Gender, DepartmentID)
-VALUES (1001, 'Arun', 'Male', 101);
 
-INSERT INTO Student (StudentID, StudentName, Gender, DepartmentID)
-VALUES (1002, 'Divya', 'Female', 102);
-
-INSERT INTO Student (StudentID, StudentName, Gender, DepartmentID)
-VALUES (1003, 'Karthik', 'Male', 101);
-
-SELECT * FROM Student;
